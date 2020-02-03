430b7ef8fe6d4d6bc76e6ae47db3e0a2e3f0f3cb
Dung <dungtrinh0702@gmail.coom>
2020-02-03 15:27:30 +0700

    nop bai tap

---
+++ [Bài tập] Game đơn giản - di chuyển đối tượng/script.js
@@ -0,0 +1,43 @@
+function Hero(image, top, left, size){
+    this.image = image;
+    this.top = top;
+    this.left = left;
+    this.size = size;
+
+    this.getHeroElement = function(){
+        return '<img width="'+ this.size + '"' +
+            ' height="'+ this.size + '"' +
+            ' src="' + this.image +'"' +
+            ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
+    };
+
+    this.moveRight = function(){
+        this.left += 50;
+        console.log('right: ' + this.left);
+    };
+
+    this.moveLeft = function () {
+        this.left -= 50;
+        console.log('left:' + this.left);
+    };
+    this.moveUp = function () {
+        this.top -= 50;
+    };
+    this.moveDown = function () {
+        thi.top += 50;
+    }
+
+}
+
+let hero = new Hero('pokemon.png', 20, 30, 200);
+
+function start(){
+    if(hero.left < window.innerWidth - hero.size){
+        hero.moveRight();
+    }
+    document.getElementById('game').innerHTML = hero.getHeroElement();
+    setTimeout(start, 500);
+
+}
+
+start();
